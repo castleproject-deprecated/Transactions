ed2b0911dc4e36bfa8caf1315382f35a8769ca7b
Henrik <henrik@haf.se>
2011-04-22 05:41:33 +0200

    now the task build_all actually builds all; I had to re-set all tasks after the first round, and that's actually exactly what I want

--- rakefile.rb
+++ rakefile.rb
@@ -7,10 +7,181 @@ require 'buildscripts/paths'
 require 'buildscripts/utils'
 require 'buildscripts/environment'
 
+# profile time: "PS \> $start = [DateTime]::UtcNow ; rake ; $end = [DateTime]::UtcNow ; $diff = $end-$start ; "Started: $start to $end, a diff of $diff"
 task :default => [:release]
-task :ci => [:release, "castle:tests", "castle:nuget_tx", "castle:nuget_autotx"]
-task :release => ["env:release", "clean", "castle:build", "castle:tests", "castle:output"]
-task :debug => ["env:debug", "clean", "castle:build", "castle:tests", "castle:output"]
+
+desc "prepare the version info files to get ready to start coding!"
+task :prepare => ["castle:assembly_infos"]
+
+desc "runner for continuous integration"
+task :ci => [":build_all", "castle:nuget"]
+
+desc "build in release mode"
+task :release => ["env:release", "clean", "castle:build"]
+
+desc "build in debug mode"
+task :debug => ["env:debug", "clean", "castle:build"]
+
+CLOBBER.include(Folders[:out])
+CLOBBER.include(Folders[:packages])
+
+Albacore.configure do |config|
+  config.nunit.command = Commands[:nunit]
+  config.nugetpack.command = Commands[:nuget]
+  config.assemblyinfo.namespaces = "System", "System.Reflection", "System.Runtime.InteropServices", "System.Security"
+end
+
+desc "Builds Debug + Release of Tx + AutoTx"
+task :build_all do
+  ["env:release", "env:debug"].each{ |t| build t }
+end
+
+def build(conf)
+  puts "BUILD ALL CONF #{conf}"
+  Rake::Task[conf].invoke # these will only be invoked once each
+  Rake::Task.tasks.each{ |t| t.reenable }
+  Rake::Task["castle:build"].invoke
+  Rake::Task["castle:test_all"].invoke
+end
+
+namespace :castle do
+
+  desc "build + tx unit tests + output"
+  task :build => ['src/TxAssemblyInfo.cs', 'src/AutoTxAssemblyInfo.cs', :msbuild, :tx_test, :output]
+  
+  desc "run all tests, also for AutoTx"
+  task :test_all => [:tx_test, :autotx_test]
+  
+  msbuild :msbuild do |msb, args|
+    # msb.use = :args[:framework] || :net40
+	config = "#{FRAMEWORK.upcase}-#{CONFIGURATION}"
+	puts "Building config #{config} with MsBuild"
+	msb.properties :Configuration => config
+    msb.targets :Clean, :Build
+    msb.solution = Files[:sln]
+  end
+  
+  directory "#{Folders[:tests]}"
+  
+  nunit :tx_test => [:msbuild, "#{Folders[:tests]}"] do |nunit|
+    nunit.command = Commands[:nunit]
+	nunit.options '/framework v4.0', 
+	  "/out #{File.join(Folders[:tests], Projects[:tx][:dir])}.log",
+	  "/xml #{File.join(Folders[:tests], Projects[:tx][:dir])}.xml"
+	nunit.assemblies Files[:tx_test]
+  end
+  
+  desc "AutoTx unit + integration tests"
+  nunit :autotx_test => [:msbuild, "#{Folders[:tests]}"] do |nunit|
+	nunit.command = Commands[:nunit]
+	nunit.options '/framework v4.0', 
+	  "/out #{File.join(Folders[:tests], Projects[:autotx][:dir])}.log",
+	  "/xml #{File.join(Folders[:tests], Projects[:autotx][:dir])}.xml"
+	nunit.assemblies Files[:autotx_test]
+  end
+  
+  task :output => [:tx_output, :autotx_output]
+  
+  task :tx_output => :msbuild do
+	target = File.join(Folders[:binaries], Projects[:tx][:dir])
+    copy_files Folders[:tx_out], "*.{xml,dll,pdb,config}", target
+    CLEAN.include(target)
+  end
+  
+  task :autotx_output => :msbuild do
+	target = File.join(Folders[:binaries], Projects[:autotx][:dir])
+	copy_files Folders[:autotx_out], "*.{xml,dll,pdb,config}", target
+	CLEAN.include(target)
+  end
+  
+  file 'src/TxAssemblyInfo.cs' => "castle:tx_version"
+  file 'src/AutoTxAssemblyInfo.cs' => "castle:autotx_version"
+  
+  task :assembly_infos => [:tx_version, :autotx_version]
+  
+  # versioning: http://support.microsoft.com/kb/556041
+  assemblyinfo :tx_version do |asm|
+	asm.product_name = asm.title = Projects[:tx][:title]
+    asm.description = Projects[:tx][:description]
+    # This is the version number used by framework during build and at runtime to locate, link and load the assemblies. When you add reference to any assembly in your project, it is this version number which gets embedded.
+	asm.version = VERSION
+	# Assembly File Version : This is the version number given to file as in file system. It is displayed by Windows Explorer. Its never used by .NET framework or runtime for referencing.
+	asm.file_version = VERSION_INFORMAL
+    asm.custom_attributes :AssemblyInformationalVersion => "#{VERSION_INFORMAL}",
+  	  :CLSCompliantAttribute => false,
+	  :AssemblyConfiguration => "#{CONFIGURATION}",
+	  :Guid => Projects[:tx][:guid]
+	asm.com_visible = false
+    asm.copyright = Projects[:tx][:copyright]
+    asm.output_file = 'src/TxAssemblyInfo.cs'
+  end
+  
+  assemblyinfo :autotx_version do |asm|
+	asm.product_name = asm.title = Projects[:autotx][:title]
+    asm.description = Projects[:autotx][:description]
+    # This is the version number used by framework during build and at runtime to locate, link and load the assemblies. When you add reference to any assembly in your project, it is this version number which gets embedded.
+	asm.version = VERSION
+	# Assembly File Version : This is the version number given to file as in file system. It is displayed by Windows Explorer. Its never used by .NET framework or runtime for referencing.
+	asm.file_version = VERSION_INFORMAL
+    asm.custom_attributes :AssemblyInformationalVersion => "#{VERSION_INFORMAL}",
+  	  :CLSCompliantAttribute => false,
+	  :AssemblyConfiguration => "#{CONFIGURATION}",
+	  :Guid => Projects[:autotx][:guid]
+    asm.copyright = Projects[:autotx][:copyright]
+    asm.output_file = 'src/AutoTxAssemblyInfo.cs'
+  end
+  
+  desc "prepare Tx Services and AutoTx Facility nuget package"
+  task :nuget => [:nuget_tx, :nuget_autotx]
+  
+  directory "#{Folders[:nuspec_tx]}"
+  file "#{Files[:nuspec_tx]}"
+  
+  nuspec :nuspec_tx => ["#{Folders[:nuspec_tx]}","#{Files[:nuspec_tx]}"] do |nuspec|
+    nuspec.id = "Castle.Services.Transaction"
+    nuspec.version = File.read(Files[:version])
+    nuspec.authors = Projects[:tx][:authors]
+    nuspec.description = Projects[:tx][:description]
+    #nuspec.working_directory = Folders[:nuspec_tx]
+    nuspec.title = Projects[:tx][:title]
+	nuspec.projectUrl = "https://github.com/haf/Castle.Services.Transaction"
+    nuspec.language = "en-US"
+    nuspec.licenseUrl = "https://github.com/haf/Castle.Services.Transaction/raw/master/License.txt"	
+    nuspec.dependency "Castle.Core", "2.5.1"
+    nuspec.output_file = Files[:nuspec_tx]
+  end
+  
+  directory "#{Folders[:nuspec_autotx]}"
+  file "#{Files[:nuspec_autotx]}"
+  
+  nuspec :nuspec_autotx => ["#{Folders[:nuspec_autotx]}", "#{Files[:nuspec_autotx]}"] do |nuspec|
+    nuspec.id = "Castle.Facilities.AutoTx"
+    nuspec.version = File.read(Files[:version])
+    nuspec.authors = Projects[:autotx][:authors]
+    nuspec.description = Projects[:autotx][:description]
+    #nuspec.working_directory = Folders[:nuspec_autotx]
+    nuspec.title = Projects[:autotx][:title]
+    nuspec.projectUrl = "https://github.com/haf/Castle.Services.Transaction"
+    nuspec.language = "en-US"
+    nuspec.licenseUrl = "https://github.com/haf/Castle.Services.Transaction/raw/master/License.txt"
+	nuspec.dependency "Castle.Core", "2.5.1"
+	nuspec.dependency "Castle.Windsor", "2.5.1"
+	nuspec.dependency "Castle.Services.Transaction", "2.5.1"
+    nuspec.output_file = Files[:nuspec_autotx]
+  end
+  
+  nugetpack :nuget_autotx => [:nuspec_autotx, :msbuild] do |nuget|
+   nuget.nuspec      = Files[:nuspec_autotx]
+   nuget.base_folder = Folders[:nuspec_autotx]
+   nuget.output      = Folders[:nuget_out]
+  end
+  
+  nugetpack :nuget_tx => [:nuspec_tx, :msbuild] do |nuget|
+   nuget.nuspec      = Files[:nuspec_tx]
+   nuget.base_folder = Folders[:nuspec_tx]
+   nuget.output      = Folders[:nuget_out]
+  end
+end
 
 desc "display rake task help"  
 task :help do
@@ -43,156 +214,4 @@ task :help do
   puts " Maintainance"
   puts " ------------"
   puts " Remove build/ dir         'rake clobber'"
-end
-
-CLOBBER.include(Folders[:out])
-CLOBBER.include(Folders[:packages])
-
-Albacore.configure do |config|
-  config.nunit.command = Commands[:nunit]
-  config.nugetpack.command = Commands[:nuget]
-  config.assemblyinfo.namespaces = "System", "System.Reflection", "System.Runtime.InteropServices", "System.Security"
-end
-
-desc "Builds both a Debug and a Release build of both Transaction Services and AutoTx Facility"
-task :build_all do
-  ["env:release", "env:debug"].each{ |t| build t }
-end
-
-def build(conf)
-  Rake::Task[conf].invoke # these will only be invoked once each
-  Rake::Task["castle:build"].reenable
-  Rake::Task["castle:build"].invoke
-end
-
-namespace :castle do
-
-  desc "build Castle Transaction Services and AutoTx Facility"
-  msbuild :build => ['src/TxAssemblyInfo.cs', 'src/AutoTxAssemblyInfo.cs'] do |msb, args|
-    # msb.use = :args[:framework] || :net40
-	config = "#{FRAMEWORK.upcase}-#{CONFIGURATION}"
-	puts "Building config #{config} with MsBuild"
-	msb.properties :Configuration => config
-    msb.targets :Build
-    msb.solution = Files[:sln]
-  end
-  
-  task :tests => [:tx_test, :autotx_test]
-  
-  nunit :tx_test => :build do |nunit|
-    puts "Testing #{Files[:tx_test]}"
-    nunit.command = Commands[:nunit]
-	nunit.options '/framework v4.0'
-	nunit.assemblies Files[:tx_test]
-  end
-  
-  nunit :autotx_test => :build do |nunit|
-    puts "Testing #{Files[:autotx_test]}"
-	nunit.command = Commands[:nunit]
-	nunit.options '/framework v4.0'
-	nunit.assemblies Files[:autotx_test]
-  end
-  
-  task :output => [:tx_output, :autotx_output]
-  
-  task :tx_output => :build do
-	target = File.join(Folders[:out], Projects[:tx][:dir])
-    copy_files Folders[:tx_out], "*.{xml,dll,pdb,config}", target
-    CLEAN.include(target)
-  end
-  
-  task :autotx_output => :build do
-	target = File.join(Folders[:out], Projects[:autotx][:dir])
-	copy_files Folders[:autotx_out], "*.{xml,dll,pdb,config}", target
-	CLEAN.include(target)
-  end
-  
-  file 'src/TxAssemblyInfo.cs' => "castle:tx_version"
-  file 'src/AutoTxAssemblyInfo.cs' => "castle:autotx_version"
-  
-  # versioning: http://support.microsoft.com/kb/556041
-  desc 'build Transaction Services assembly info file'
-  assemblyinfo :tx_version do |asm|
-	asm.product_name = asm.title = Projects[:tx][:title]
-    asm.description = Projects[:tx][:description]
-    # This is the version number used by framework during build and at runtime to locate, link and load the assemblies. When you add reference to any assembly in your project, it is this version number which gets embedded.
-	asm.version = VERSION
-	# Assembly File Version : This is the version number given to file as in file system. It is displayed by Windows Explorer. Its never used by .NET framework or runtime for referencing.
-	asm.file_version = VERSION_INFORMAL
-    asm.custom_attributes :AssemblyInformationalVersion => "#{VERSION_INFORMAL}",
-  	  :CLSCompliantAttribute => false,
-	  :AssemblyConfiguration => "#{CONFIGURATION}",
-	  :Guid => Projects[:tx][:guid]
-	asm.com_visible = false
-    asm.copyright = Projects[:tx][:copyright]
-    asm.output_file = 'src/TxAssemblyInfo.cs'
-  end
-  
-  desc 'build AutoTx Facility assembly info file'
-  assemblyinfo :autotx_version do |asm|
-	asm.product_name = asm.title = Projects[:autotx][:title]
-    asm.description = Projects[:autotx][:description]
-    # This is the version number used by framework during build and at runtime to locate, link and load the assemblies. When you add reference to any assembly in your project, it is this version number which gets embedded.
-	asm.version = VERSION
-	# Assembly File Version : This is the version number given to file as in file system. It is displayed by Windows Explorer. Its never used by .NET framework or runtime for referencing.
-	asm.file_version = VERSION_INFORMAL
-    asm.custom_attributes :AssemblyInformationalVersion => "#{VERSION_INFORMAL}",
-  	  :CLSCompliantAttribute => false,
-	  :AssemblyConfiguration => "#{CONFIGURATION}",
-	  :Guid => Projects[:autotx][:guid]
-    asm.copyright = Projects[:autotx][:copyright]
-    asm.output_file = 'src/AutoTxAssemblyInfo.cs'
-  end
-  
-  directory "#{Folders[:nuspec_tx]}"
-  file "#{Files[:nuspec_tx]}"
-  
-  desc "nuget specification for Transaction Services"
-  nuspec :nuspec_tx => ["#{Folders[:nuspec_tx]}","#{Files[:nuspec_tx]}"] do |nuspec|
-    nuspec.id = "Castle.Services.Transaction"
-    nuspec.version = File.read(Files[:version])
-    nuspec.authors = Projects[:tx][:authors]
-    nuspec.description = Projects[:tx][:description]
-    #nuspec.working_directory = Folders[:nuspec_tx]
-    nuspec.title = Projects[:tx][:title]
-	nuspec.projectUrl = "https://github.com/haf/Castle.Services.Transaction"
-    nuspec.language = "en-US"
-    nuspec.licenseUrl = "https://github.com/haf/Castle.Services.Transaction/raw/master/License.txt"	
-    nuspec.dependency "Castle.Core", "2.5.1"
-    nuspec.output_file = Files[:nuspec_tx]
-  end
-  
-  directory "#{Folders[:nuspec_autotx]}"
-  file "#{Files[:nuspec_autotx]}"
-  
-  desc "nuget specification for AutoTx Facility"
-  nuspec :nuspec_autotx => ["#{Folders[:nuspec_autotx]}", "#{Files[:nuspec_autotx]}"] do |nuspec|
-    nuspec.id = "Castle.Facilities.AutoTx"
-    nuspec.version = File.read(Files[:version])
-    nuspec.authors = Projects[:autotx][:authors]
-    nuspec.description = Projects[:autotx][:description]
-    #nuspec.working_directory = Folders[:nuspec_autotx]
-    nuspec.title = Projects[:autotx][:title]
-    nuspec.projectUrl = "https://github.com/haf/Castle.Services.Transaction"
-    nuspec.language = "en-US"
-    nuspec.licenseUrl = "https://github.com/haf/Castle.Services.Transaction/raw/master/License.txt"
-	nuspec.dependency "Castle.Core", "2.5.1"
-	nuspec.dependency "Castle.Windsor", "2.5.1"
-	nuspec.dependency "Castle.Services.Transaction", "2.5.1"
-    nuspec.output_file = Files[:nuspec_autotx]
-  end
-  
-  desc "create AutoTx nuget package"
-  nugetpack :nuget_autotx => :nuspec_autotx do |nuget|
-   nuget.nuspec      = Files[:nuspec_autotx]
-   nuget.base_folder = Folders[:nuspec_autotx]
-   nuget.output      = Folders[:nuget_out]
-  end
-  
-  desc "create Tx Services nuget package"
-  nugetpack :nuget_tx => :nuspec_tx do |nuget|
-   nuget.nuspec      = Files[:nuspec_tx]
-   nuget.base_folder = Folders[:nuspec_tx]
-   nuget.output      = Folders[:nuget_out]
-  end
 end
