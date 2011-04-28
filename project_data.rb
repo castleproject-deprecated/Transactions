901d989ca52147c3f4e46c99400bc7654a85f655
Henrik <henrik@haf.se>
2011-04-28 19:41:20 +0200

    Finished tutorial

---
+++ project_data.rb
@@ -0,0 +1,14 @@
+# the Projects hash should only contain strings relating to the projects.
+Projects = {
+	:proj_key => {	
+		:id => "Castle.XXX",
+		:dir => "Castle.XXX",
+		:test_dir => "Castle.XXX.Tests",
+		:title => "Castle XXX",
+		:description => "XXX",
+		:copyright => "Apache 2.0 Licensed. The Castle Project 2004-2011",
+		:authors => "Castle Project Committers and Contributers",
+		:company => "The Castle Project",
+		:guid => "xxxxxxxx-fa2c-4630-a087-abd28f77af28"
+	}
+}
