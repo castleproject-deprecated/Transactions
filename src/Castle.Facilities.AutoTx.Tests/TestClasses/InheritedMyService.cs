363cbb9ecd382f49231e66f4bc25ff6581051430
Martin Mudra <mudra.martin@gmail.com>
2015-03-23 14:47:58 +0100

    changed whitespace

--- src/Castle.Facilities.AutoTx.Tests/TestClasses/InheritedMyService.cs
+++ src/Castle.Facilities.AutoTx.Tests/TestClasses/InheritedMyService.cs
@@ -2,10 +2,10 @@
 
 namespace Castle.Facilities.AutoTx.Tests.TestClasses
 {
-    public class InheritedMyService:MyService
-    {
-        public InheritedMyService(ITransactionManager manager) : base(manager)
-        {
-        }
-    }
+	public class InheritedMyService:MyService
+	{
+		public InheritedMyService(ITransactionManager manager) : base(manager)
+		{
+		}
+	}
 }
