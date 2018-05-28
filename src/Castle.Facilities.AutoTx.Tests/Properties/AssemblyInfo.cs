eda9d78468e2de471114a5af1be18c2e760f02fe
Vladimír Pokorný <vladimir.pokorny@scalesoft.cz>
2018-05-28 09:26:30 +0200

    Remove dependencies to generated AssemblyInfo

---
+++ src/Castle.Facilities.AutoTx.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+[assembly: AssemblyTitle("Castle AutoTx Facility")]
+[assembly: AssemblyDescription("This facility enables ambient easy-to-use declarative transactions through the [Transaction] attribute. This is the glue that holds the bits and pieces from Castle Transactions together and the stuff that will make your co-workers googly-eyed and impressed.")]
+[assembly: AssemblyProduct("Castle AutoTx Facility")]
+[assembly: AssemblyCopyright("Apache 2.0 Licensed. (c) The Castle Project 2004-2011")]
+[assembly: AssemblyVersion("3.3.0.14213")]
+[assembly: AssemblyFileVersion("3.3.0.14213")]
+
+[assembly: AssemblyInformationalVersion("3.3.0.14213")]
+[assembly: CLSCompliantAttribute(false)]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: Guid("BBDD1C0A-67E9-48C7-9A14-69CB8B158549")]
---
+++ src/Castle.Transactions.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Security;
+[assembly: AssemblyTitle("Castle Transactions")]
+[assembly: AssemblyDescription("Castle Transactions enables a common interface to both .Net Transactions with LTM/Lightweight Transaction Manager. be9137445010dab8fee890d9ba46b83400b3cd8c - 2018-05-22 113642")]
+[assembly: AssemblyProduct("Castle Transactions")]
+[assembly: AssemblyCopyright("Apache 2.0 Licensed. (c) The Castle Project 2004-2011")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("3.3.0.14213")]
+[assembly: AssemblyFileVersion("3.3.0.14213")]
+
+[assembly: AssemblyInformationalVersion("3.3.0.14213")]
+[assembly: CLSCompliantAttribute(false)]
+[assembly: AssemblyConfiguration("Release")]
+[assembly: Guid("67CDFD3A-450F-4CD4-9401-EF15361E2713")]
